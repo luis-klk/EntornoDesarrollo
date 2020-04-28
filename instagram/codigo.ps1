0c7e8191e0e1ce596c39d95f79cb54e9592b1b9f
Luis heredia <55788670+luis-klk@users.noreply.github.com>
2020-04-28 13:37:51 +0200

    28/04

---
+++ instagram/codigo.ps1
@@ -0,0 +1,41 @@
+﻿
+$MouseEventSig=@'
+[DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@
+$MouseEvent = Add-Type -memberDefinition $MouseEventSig -name "MouseEventWinApi" -passThru
+
+$usuario="powershellprueba"
+$contraseña="prueba1"
+start-process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList 'https://www.instagram.com/explore/'
+$i = 3
+while($i -ge 0){
+    $i
+Start-Sleep -Seconds 2
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(320,400)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+Start-Sleep -Seconds 2
+
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(520,400)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+Start-Sleep -Seconds 2
+
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(720,400)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+Start-Sleep -Seconds 2
+
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(920,400)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+Start-Sleep -Seconds 2
+
+[System.Windows.Forms.SendKeys]::SendWait("^({F5})")
+Start-Sleep -Seconds 2
+    $i--
+}
+
+
+start-process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList ('https://www.instagram.com/'+$usuario+'/following/')
